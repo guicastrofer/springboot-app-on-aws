5ba720a7f4d60764c1cb02189f74f9385f3e2391
Guilherme de Castro <42876310+guicastrofer@users.noreply.github.com>
2020-07-21 17:18:01 -0300

    My first commit

---
+++ src/main/java/com/aws/service/entity/Machine.java
@@ -0,0 +1,24 @@
+package com.aws.service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Machine implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String ip;
+    private String name;
+    private String os;
+    private String architecture;
+    private String osVersion;
+
+}
